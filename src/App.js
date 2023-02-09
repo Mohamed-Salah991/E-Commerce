749ae8a5f0a79eb193d02e8a009626fa26e2558a
Mohamed <mohamed.salah6486@gmail.com>
2023-02-10 00:57:30 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1>Welcome Back with E-commerce Website </h1>;
+  return (
+    <h1>
+      Welcome Back with E-commerce Website <h2>Updated Second </h2>
+    </h1>
+  );
 }
 
 export default App;
