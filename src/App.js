80284cbf0fe3f2c0df6ab3f9ec835ac770e6ac11
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-02-28 21:17:49 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
+import { useState, useEffect, useCallback } from "react";
 import Home from "./Pages/Home/Home";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      setProducts(data);
+      return data;
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(products);
+
   return (
     <>
       <Header />
--- src/components/Slider/BtnSlider.js
+++ src/components/Slider/BtnSlider.js
@@ -4,7 +4,7 @@ import leftArrow from "./icons/left-arrow.svg";
 import rightArrow from "./icons/right-arrow.svg";
 
 export default function BtnSlider({ direction, moveSlide }) {
-  console.log(direction, moveSlide);
+  // console.log(direction, moveSlide);
   return (
     <button
       onClick={moveSlide}
