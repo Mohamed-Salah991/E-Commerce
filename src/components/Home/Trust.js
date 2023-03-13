be7a1676654d52307c262775168668d1f4a4f4dc
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-03-13 23:56:09 +0200

    update

--- src/components/Home/Trust.js
+++ src/components/Home/Trust.js
@@ -2,7 +2,13 @@ import React from "react";
 import classes from "./Trust.module.css";
 
 function Trust() {
-  return <div className={classes.trust}></div>;
+  return (
+    <div className={classes.trust}>
+      <div className={classes.content}>
+        <div className={classes["box-info"]}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Trust;
