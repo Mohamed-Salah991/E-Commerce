f568000d3bd36ac688e83c48cbc9632862a0f6b7
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-03-18 23:53:40 +0200

    slove the grid fixed width in small screens

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -13,62 +13,60 @@ function Header() {
   const test = 9;
   return (
     <nav className={classes.header}>
-      <div className="container">
-        <section className={classes["small-screen"]}>
-          <div className={classes["bottom-icons"]}>
-            <ul>
-              <li>
-                <AiOutlineHome />
-                <span>Home</span>
-              </li>
-              <li>
-                <BsCart4 />
-                <span>Cart</span>
-              </li>
-              <li>
-                <AiOutlineHeart />
-                <span>Whish List</span>
-              </li>
-              <li>
-                <AiOutlineUser />
-                <span>Account</span>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className={classes.up}>
-          <div className={classes.logo}>
-            <a href="/">
-              <img src={logo} alt="" />
-            </a>
-          </div>
-          <div className={classes.search}>
-            <Search />
-          </div>
-          <div className={classes.icons}>
-            <ul>
-              <li>
-                <FiBarChart2 className={classes.icon} />
-                <span>5</span>
-              </li>
-              <li>
-                <AiOutlineHeart className={classes.icon} />
-                <span>5</span>
-              </li>
+      <section className={classes["small-screen"]}>
+        <div className={classes["bottom-icons"]}>
+          <ul>
+            <li>
+              <AiOutlineHome />
+              <span>Home</span>
+            </li>
+            <li>
+              <BsCart4 />
+              <span>Cart</span>
+            </li>
+            <li>
+              <AiOutlineHeart />
+              <span>Whish List</span>
+            </li>
+            <li>
+              <AiOutlineUser />
+              <span>Account</span>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={classes.up}>
+        <div className={classes.logo}>
+          <a href="/">
+            <img src={logo} alt="" />
+          </a>
+        </div>
+        <div className={classes.search}>
+          <Search />
+        </div>
+        <div className={classes.icons}>
+          <ul>
+            <li>
+              <FiBarChart2 className={classes.icon} />
+              <span>5</span>
+            </li>
+            <li>
+              <AiOutlineHeart className={classes.icon} />
+              <span>5</span>
+            </li>
 
-              <li>
-                <BsCart4 className={classes.icon} />
-                <span>{test}</span>
-              </li>
-              <li>
-                <AiOutlineUser className={classes.icon} />
-              </li>
-            </ul>
-            <h4 className={["use-auth"]}>Login</h4>
-          </div>
-        </section>
-        <section className={classes.down}></section>
-      </div>
+            <li>
+              <BsCart4 className={classes.icon} />
+              <span>{test}</span>
+            </li>
+            <li>
+              <AiOutlineUser className={classes.icon} />
+            </li>
+          </ul>
+          <h4 className={["use-auth"]}>Login</h4>
+        </div>
+      </section>
+      <section className={classes.down}></section>
     </nav>
   );
 }
