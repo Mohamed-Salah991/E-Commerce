9fe1d46b6117498545c0f262f6734b2e20662ec1
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-03-20 23:42:36 +0200

    add cart

---
+++ src/components/Cart/CartController.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CartController.module.css";
+
+function CartController() {
+  return <div>CartController</div>;
+}
+
+export default CartController;
