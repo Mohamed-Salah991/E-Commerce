dd6ced5aa1921dfa1fdb41ac4afd4745f0a9cb43
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-03-20 23:52:19 +0200

    add cart item

--- src/components/Cart/CartController.js
+++ src/components/Cart/CartController.js
@@ -1,8 +1,15 @@
 import React from "react";
 import classes from "./CartController.module.css";
+import CartItem from "./CartItem";
 
 function CartController() {
-  return <div className={classes["CartController"]}>CartController</div>;
+  return (
+    <div className={classes["CartController"]}>
+      <div className={classes.content}>
+        <CartItem />
+      </div>
+    </div>
+  );
 }
 
 export default CartController;
